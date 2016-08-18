6a3b1b377b0b9ab873535af2609772896b72fa5a
html5col <html5master@sina.com>
2016-08-18 18:30:24 +0800

    second-level domain: add cors module

--- secondDomain/api.js
+++ secondDomain/api.js
@@ -1,18 +1,33 @@
+//api.frank:8050/login refers to localhost:8050/login? why?
+//seems it need to overwrite the same route as localhost
 "use strict";
-const vhost = require('vhost');
+const vhost = require('vhost'),
+      //it's fine to load outside resources. we use CORS principle only when we use ajax to load resources
+      cors  = require('cors'),
+      corsOptions = {
+           origin: 'http:///frank'//http://trver.com
+      };
 
 module.exports = (app,express)=>{
-	
+
 	var api = express.Router();
 
     app.use(vhost('api.frank', api));
 
+    //do not use app.use(require('cors')()) in order to prevent attack,
+    //cors is achieved by Access-Control-Allow-Origin header
+    app.use('api.frank', require('cors')(corsOptions));
+
     api.get('/', (req,res)=>{
-    	res.render('cross-browser-test/hood-river');
+    	res.json({page: 'this is the home page of the api'});
     });	
     
     api.get('/about', (req,res)=>{
-    	res.render('home/about');
+    	res.json({page: 'this is the about page of the api'});
     });	
+
+    // api.get('/signup', (req,res)=>{
+    // 	res.render('home/about');
+    // });	
 
 } 
