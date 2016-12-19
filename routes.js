8d1ea1c4988a78f3599fc044452242f151af5885
html5col <html5master@sina.com>
2016-12-19 10:26:37 +0800

    delete anything about group in routes.js

--- routes.js
+++ routes.js
@@ -6,8 +6,7 @@ const user = require('./routes/user'),
 	  login3 = require('./routes/login3'),
 	  index = require('./routes/index'),
 	  api = require('./routes/api'),
-	  post = require('./routes/post'),
-	  group = require('./routes/group');
+	  post = require('./routes/post');
 
 module.exports   = function(app, passport,User) {
 	 app.use('/',index);  
@@ -15,9 +14,8 @@ module.exports   = function(app, passport,User) {
 	 app.use('/user',user(app,User,passport));
 	 app.use('/api',api);
 	 app.use('/auth',login3);
-     app.use('/post', post);
+   app.use('/post', post);
 	 app.use('/test',test);
-	 app.use('/group',group);
 
 		//to get form data using req.body
 		/*****form part end********/
